b8a66e25b6b1204bd38dcee52b6caa212be89a33
Jayde Livingstone <livingst@slurm-nice-devel1903.esrf.fr>
2023-01-09 11:59:06 +0100

    Calculate and plot NPS

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from random import randint
 from PIL import Image
 from skimage.metrics import structural_similarity
+import matplotlib.colors as mcolors
 
 ref_filepath = sys.argv[1]
 test_filepath = sys.argv[2]
@@ -17,7 +18,8 @@ n_roi = int(sys.argv[5])
 roi_type = sys.argv[6]
 roi_halfwidth = int(sys.argv[7])
 do_nps = sys.argv[8]
-save_filepath = sys.argv[9]
+px_size = float(sys.argv[9])
+save_filepath = sys.argv[10]
 
 if roi_type == "circle" or roi_type == "Circle" or roi_type == "circ":
 	roi = "circle"
@@ -110,18 +112,33 @@ def select_pixel(event, x, y, flags, params):
 		c2 = randint(0, 255)
 		c3 = randint(0, 255)
 		
-def nps_calc():
-	print("calculating nps")
+def nps_calc(roi_raw, roi_mean, r_sp_freq):
 	# For each ROI:
 	# Get mean value
 	# Subtract mean value from raw values
 	# DFT the result
 	# Square the result
-	# Bin the data according to radial spatial frequency (np.digitize)
+	# Bin the data according to radial spatial frequency
 	# Average the data in each bin
+	# Multiply by ratio of pixel area to ROI area
 	# Then (outside this function):
 	# Average over all ROIs
-	# Multiply by ratio of pixel area to ROI area
+			
+	g = roi_raw - roi_mean
+	fft_g = np.fft.fft2(g)
+	fft_g_sq = np.power(np.absolute(fft_g), 2)
+	
+	dim = roi_raw.shape[0]
+	nbins = int(dim/2)
+	
+	bin_sum, bin_edges = np.histogram(r_sp_freq, bins=nbins, density=False, weights=fft_g_sq)
+	bin_counts, bin_edges = np.histogram(r_sp_freq, bins=nbins, density=False, weights=None)
+	bin_ave = bin_sum / bin_counts
+	bin_centres = [(bin_edges[x] + bin_edges[x+1])/2 for x in range(len(bin_edges)-1)]
+	bin_nps = (px_size**2 / dim**2) * bin_ave # units: rad^2 nm^2
+	
+	return bin_centres, bin_nps
+			
 	
 
 """
@@ -147,6 +164,8 @@ if len(ref_images) != len(test_images):
 else:
 	n_slice = len(ref_images)
 	
+list_of_colours = list(mcolors.TABLEAU_COLORS.keys())
+	
 results = dataFrame()
 
 for n in range(n_slice):
@@ -166,8 +185,9 @@ for n in range(n_slice):
 	
 	while displayFlag == True:
 		if len(roi_x) > 0:
-			ref_img.image_markup(str(len(roi_x)-1), (roi_x[-1],roi_y[-1]), int(roi_halfwidth*scale_display), (c1,c2,c3), 2)
-			test_img.image_markup(str(len(roi_x)-1), (roi_x[-1],roi_y[-1]), int(roi_halfwidth*scale_display), (c1,c2,c3), 2)
+			rgb = (np.asarray(mcolors.to_rgb(mcolors.TABLEAU_COLORS[list_of_colours[len(roi_x)-1]]))*255).astype(int)
+			ref_img.image_markup(str(len(roi_x)-1), (roi_x[-1],roi_y[-1]), int(roi_halfwidth*scale_display), (int(rgb[2]), int(rgb[1]), int(rgb[0])), 2)
+			test_img.image_markup(str(len(roi_x)-1), (roi_x[-1],roi_y[-1]), int(roi_halfwidth*scale_display), (int(rgb[2]), int(rgb[1]), int(rgb[0])), 2)
 		ref_img.display()
 		cv2.setMouseCallback(ref_img.displayText, select_pixel)
 		cv2.waitKey(1)
@@ -176,9 +196,9 @@ for n in range(n_slice):
 			cv2.destroyAllWindows()
 			displayFlag = False
 			
-			
-	ref_img.image_markup(str(len(roi_x)-1), (roi_x[-1],roi_y[-1]), int(roi_halfwidth*scale_display), (c1,c2,c3), 2)
-	test_img.image_markup(str(len(roi_x)-1), (roi_x[-1],roi_y[-1]), int(roi_halfwidth*scale_display), (c1,c2,c3), 2)
+	rgb = (np.asarray(mcolors.to_rgb(mcolors.TABLEAU_COLORS[list_of_colours[len(roi_x)-1]]))*255).astype(int)
+	ref_img.image_markup(str(len(roi_x)-1), (roi_x[-1],roi_y[-1]), int(roi_halfwidth*scale_display), (int(rgb[2]), int(rgb[1]), int(rgb[0])), 2)
+	test_img.image_markup(str(len(roi_x)-1), (roi_x[-1],roi_y[-1]), int(roi_halfwidth*scale_display), (int(rgb[2]), int(rgb[1]), int(rgb[0])), 2)
 	
 	joined_disp = np.concatenate((ref_img.mkup_img, test_img.mkup_img), axis=1)
 	joined_disp = cv2.resize(joined_disp, (2*ref_img.disp_cols, ref_img.disp_rows))
@@ -215,11 +235,44 @@ for n in range(n_slice):
 		elif roi == "square":
 			ref_values = ref_img.data[y0-roi_halfwidth:y0+roi_halfwidth, x0-roi_halfwidth:x0+roi_halfwidth]
 			test_values = test_img.data[y0-roi_halfwidth:y0+roi_halfwidth, x0-roi_halfwidth:x0+roi_halfwidth]
+			
 					
 		ref_mean = np.mean(ref_values)
 		ref_sd = np.std(ref_values)
 		test_mean = np.mean(test_values)
 		test_sd = np.std(test_values)
+		
+		if do_nps:
+			if idx==0:
+				print("Calculating NPS.")
+				fig=plt.figure()
+				ref_plt = fig.gca()
+				test_plt = fig.gca()
+				
+				sp_freq = np.fft.fftfreq(ref_values.shape[0], px_size)
+				df = sp_freq[1] - sp_freq[0]
+				sp_freq = sp_freq + df/2
+	
+				r = np.zeros(ref_values.shape)
+	
+				for i in range(len(sp_freq)):
+					for j in range(len(sp_freq)):
+						yy = sp_freq[i]
+						xx = sp_freq[j]
+						r[i, j] = np.sqrt(xx**2 + yy**2)
+		
+			ref_sp_freq, ref_noise_power = nps_calc(ref_values, ref_mean, r)
+			test_sp_freq, test_noise_power = nps_calc(test_values, test_mean, r)
+			
+			roi_colour = list_of_colours[idx]
+			
+			ref_plt.plot(ref_sp_freq, ref_noise_power, color=roi_colour, linestyle='--', label="Ref ROI "+str(idx))
+			test_plt.plot(test_sp_freq, test_noise_power, color=roi_colour, linestyle='-', label="Test ROI "+str(idx))
+			
+			if idx==n_roi-1:
+				plt.xlabel(r"Spatial frequency (nm$^{-1}$)")
+				plt.ylabel(r"Noise power (rad$^{2}$ nm$^{2}$)")
+				plt.savefig(save_filepath + "/figures/nps_" + "ref_" + ref_img.sliceName + "_test_" + test_img.sliceName + ".png")
 		
 		data_to_write = [ref_img.sliceName, test_img.sliceName, idx, (roi_x[idx], roi_y[idx]), ref_mean, ref_sd, test_mean, test_sd]
 		results.idx = (n * n_roi) + idx
